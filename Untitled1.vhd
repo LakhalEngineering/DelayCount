2cfbba08cac5e3537a1bfaacde702a9cc358224a
LakhalEngineering <125914042+LakhalEngineering@users.noreply.github.com>
2023-04-17 22:41:03 +0200

    Create Untitled1.vhd
    Init commit

---
+++ Untitled1.vhd
@@ -0,0 +1,10 @@
+Delay(IN:=TRUE, PT:=T#1S); (* Delay *)
+		IF NOT(Delay.Q) THEN
+ 			  RETURN;
+		END_IF
+		Delay(IN:=FALSE);
+		Count := Count +1;
+			IF Count = 5 THEN (* Set un delay di 5s *)
+                (*code*)
+            END_IF
+
